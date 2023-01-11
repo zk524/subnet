0775965985330d05401cce91372dbfe76a88fa29
zk524 <20262010007@fudan.edu.cn>
2023-01-11 23:13:58 +0800

    init

---
+++ admin.sh
@@ -0,0 +1,44 @@
+# !/bin/sh
+
+URL="http://localhost:9993/controller/network/"
+NODEID=$(cat /var/lib/zerotier-one/identity.public | cut -d ":" -f 1)
+TOKEN=$(cat /var/lib/zerotier-one/authtoken.secret)
+NWID="${NODEID}000000"
+
+if [[ $1 = "lsnet" ]]; then
+    # dc exec zerotier sh zt lsnet
+    curl $URL -H X-ZT1-AUTH:$TOKEN -s
+    
+    elif [[ $1 = "lsusr" ]]; then
+    # dc exec zerotier sh zt lsusr
+    curl ${URL} -H X-ZT1-AUTH:${TOKEN} -s
+    
+    elif [[ $1 = "auth" ]]; then
+    # dc exec zerotier sh zt auth [MEMID]
+    curl -X POST ${URL}${NWID}/member/$2 -H X-ZT1-AUTH:${TOKEN} -d '{"authorized": true}' -s
+    
+    elif [[ $1 = "delauth" ]]; then
+    # dc exec zerotier sh zt delauth [MEMID]
+    curl -X POST ${URL}${NWID}/member/$2 -H X-ZT1-AUTH:${TOKEN} -d '{"authorized": false}' -s
+    
+    elif [[ $1 = "setip" ]]; then
+    # dc exec zerotier sh zt setip [MEMID] [IP]
+    curl -X POST ${URL}${NWID}/member/$2 -H X-ZT1-AUTH:${TOKEN} -d '{"ipAssignments":['\""$3"\"']}' -s
+    
+    elif [[ $1 = "addnet" ]]; then
+    # dc exec zerotier sh zt addnet '{"name":"net"}'
+    curl -X POST ${URL}${NODEID}______ -H X-ZT1-AUTH:${TOKEN} -d $2 -s
+    
+    elif [[ $1 = "delnet" ]]; then
+    # dc exec zerotier sh zt delnet [NWID]
+    curl -X DELETE ${URL}$2 -H X-ZT1-AUTH:${TOKEN} -s
+    
+    elif [[ $1 = "delnetall" ]]; then
+    # dc exec zerotier sh zt delnetall
+    for nwid in $(curl ${URL} -H X-ZT1-AUTH:${TOKEN} -s |xargs|sed 's/\[//g;s/\]//g;s/,/\n/g')
+    do
+        if [ ! $nwid = $NWID ] ; then
+            curl -X DELETE ${URL}${nwid} -H X-ZT1-AUTH:${TOKEN} -s|sed -n '/nwid/p'
+        fi
+    done
+fi
---
+++ init.sh
@@ -0,0 +1,33 @@
+# !/bin/sh
+
+cd /var/lib/zerotier-one
+chmod +x admin.sh
+ln -s $PWD/admin.sh /zt
+
+apk add --no-cache --allow-untrusted curl
+if [ ! -f zerotier-one.pid ]; then
+    apk add --no-cache --allow-untrusted g++
+    mkdir tmp
+    mkdir -p controller.d/network
+    zerotier-idtool generate identity.secret identity.public
+    addr=$(curl ip.sb -s)/9993
+    identity=`cat identity.public`
+    NODEID=`echo $identity|cut -d ":" -f 1`
+    curl https://codeload.github.com/zerotier/ZeroTierOne/tar.gz/refs/tags/1.8.6 --output tmp/z.tar.gz
+    tar xzf tmp/z.tar.gz -C tmp --strip-components 1
+    cd tmp/attic/world
+    sed -i '/roots.push_back/d;/roots.back()/d' ./mkworld.cpp
+    sed -i '85i roots.push_back(World::Root());\nroots.back().identity = Identity(\"'"$identity"'\");\nroots.back().stableEndpoints.push_back(InetAddress(\"'"$addr"'\"));' ./mkworld.cpp
+    echo Building world.bin...
+    sh ./build.sh && ./mkworld
+    mv ./world.bin ../../../planet
+    cd -
+    apk del g++
+    rm -rf tmp
+    rm -rf /var/cache/apk/*
+    rm -rf /root/.cache
+    echo "{\"settings\": {\"portMappingEnabled\": true,\"softwareUpdate\": \"disable\",\"allowManagementFrom\": [\"0.0.0.0/0\"]}}" > local.conf
+    echo "{\"authTokens\":[null],\"authorizationEndpoint\":\"\",\"capabilities\":[],\"clientId\":\"\",\"creationTime\":1600000000000,\"dns\":[],\"enableBroadcast\":true,\"id\":\"${NODEID}000000\",\"ipAssignmentPools\":[{\"ipRangeEnd\":\"172.30.0.254\",\"ipRangeStart\":\"172.30.0.1\"}],\"mtu\":2800,\"multicastLimit\":32,\"name\":\"net\",\"nwid\":\"${NODEID}000000\",\"objtype\":\"network\",\"private\":true,\"remoteTraceLevel\":0,\"remoteTraceTarget\":null,\"revision\":1,\"routes\":[{\"target\":\"172.30.0.0/24\",\"via\":null}],\"rules\":[{\"etherType\":2048,\"not\":true,\"or\":false,\"type\":\"MATCH_ETHERTYPE\"},{\"etherType\":2054,\"not\":true,\"or\":false,\"type\":\"MATCH_ETHERTYPE\"},{\"etherType\":34525,\"not\":true,\"or\":false,\"type\":\"MATCH_ETHERTYPE\"},{\"type\":\"ACTION_DROP\"},{\"type\":\"ACTION_ACCEPT\"}],\"rulesSource\":\"\",\"ssoEnabled\":false,\"tags\":[],\"v4AssignMode\":{\"zt\":true},\"v6AssignMode\":{\"6plane\":false,\"rfc4193\":false,\"zt\":false}}" > controller.d/network/${NODEID}000000.json
+    echo Nodeid: $NODEID
+fi
+zerotier-one -U
