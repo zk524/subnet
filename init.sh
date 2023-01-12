12829ab552c1f38e289a27d0a1ab8b57fe9d7a32
zk524 <20262010007@fudan.edu.cn>
2023-01-12 09:51:23 +0800

    separate

--- init.sh
+++ init.sh
@@ -1,15 +1,9 @@
 # !/bin/sh
 
 cd /var/lib/zerotier-one
-
 apk add --no-cache --allow-untrusted curl
-if [ -f admin.sh ]; then
-    chmod +x admin.sh
-    ln -s $PWD/admin.sh /zt
-fi
-if [ ! -f identity.secret ]; then
-    zerotier-idtool generate identity.secret identity.public
-fi
+
+if [ ! -f identity.secret ]; then zerotier-idtool generate identity.secret identity.public; fi
 if [ ! -f planet ]; then
     apk add --no-cache --allow-untrusted g++
     mkdir tmp
@@ -34,4 +28,6 @@ if [ ! -f planet ]; then
     echo "{\"authTokens\":[null],\"authorizationEndpoint\":\"\",\"capabilities\":[],\"clientId\":\"\",\"creationTime\":1600000000000,\"dns\":[],\"enableBroadcast\":true,\"id\":\"${NODEID}000000\",\"ipAssignmentPools\":[{\"ipRangeEnd\":\"172.30.0.254\",\"ipRangeStart\":\"172.30.0.1\"}],\"mtu\":2800,\"multicastLimit\":32,\"name\":\"net\",\"nwid\":\"${NODEID}000000\",\"objtype\":\"network\",\"private\":true,\"remoteTraceLevel\":0,\"remoteTraceTarget\":null,\"revision\":1,\"routes\":[{\"target\":\"172.30.0.0/24\",\"via\":null}],\"rules\":[{\"etherType\":2048,\"not\":true,\"or\":false,\"type\":\"MATCH_ETHERTYPE\"},{\"etherType\":2054,\"not\":true,\"or\":false,\"type\":\"MATCH_ETHERTYPE\"},{\"etherType\":34525,\"not\":true,\"or\":false,\"type\":\"MATCH_ETHERTYPE\"},{\"type\":\"ACTION_DROP\"},{\"type\":\"ACTION_ACCEPT\"}],\"rulesSource\":\"\",\"ssoEnabled\":false,\"tags\":[],\"v4AssignMode\":{\"zt\":true},\"v6AssignMode\":{\"6plane\":false,\"rfc4193\":false,\"zt\":false}}" > controller.d/network/${NODEID}000000.json
     echo Nodeid: $NODEID
 fi
+
+chmod +x /z
 zerotier-one -U
--- readme.md
+++ readme.md
@@ -15,22 +15,21 @@
   - `docker compose up -d`
 
 - Use ([Doc](https://docs.zerotier.com/self-hosting/network-controllers))
-  - List Networks: `docker exec zerotier sh zt lsnet`
-  - List Network Members: `docker exec zerotier sh zt lsusr [NWID]`
-  - Authorize a member: `docker exec zerotier sh zt auth [MEMID]`
-  - Set member ip: `docker exec zerotier sh zt setip [MEMID] [IP]`
-  - Create Network: `docker exec zerotier sh zt addnet '{}'`
-  - Delete Network: `docker exec zerotier sh zt delnet [NWID]`
+  - List Networks: `docker exec zerotier sh z lsnet`
+  - List Network Members: `docker exec zerotier sh z lsusr [NWID]`
+  - Authorize a member: `docker exec zerotier sh z auth [MEMID]`
+  - Set member ip: `docker exec zerotier sh z setip [MEMID] [IP]`
+  - Create Network: `docker exec zerotier sh z addnet '{}'`
+  - Delete Network: `docker exec zerotier sh z delnet [NWID]`
 
 ## Client
 
 - Start
-  - Copy the _planet_ from Controller to the local dir
   - `docker compose -f docker-compose.client.yml up -d`
-
-## Note
-
-- Dont't startup the Controller and Client in a same folder or a same port(9993).
+- Use
+  - MemberID: `docker exec zerotier-client zerotier-cli info`
+  - Join network: `docker exec zerotier-client zerotier-cli join [NWID]`, then Controller authorize this member by **MemberID**.
+  - List Peers: `docker exec zerotier-client zerotier-cli listpeers`
 
 [contributors-shield]: https://img.shields.io/github/contributors/zk524/zerotier-custom.svg?style=for-the-badge
 [contributors-url]: https://github.com/zk524/zerotier-custom/graphs/contributors
